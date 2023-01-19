69fe0b27a86a1133201e2fe762d98308ac4ab80c
Matteo Rigon <matteo.rigon7@gmail.com>
2023-01-19 15:41:34 +0100

    restore entrypoints

--- entrypoints.js
+++ entrypoints.js
@@ -1,2 +1,2 @@
 // Number of entries for each entrypoint
-module.exports = [500, 1000];
+module.exports = [500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 250000, 500000, 1000000, 2000000];
